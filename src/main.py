5e102eb4fa4136808acce0770640b7681281caad
dazzlemon <daanill13470@gmail.com>
2021-01-25 09:48:20 +0200

    responsive axises

--- src/main.py
+++ src/main.py
@@ -2,19 +2,20 @@ import pygame
 
 
 class Plot:
-    def __init__(self, displaySize, bgColor, range_):
+    def __init__(self, displaySize, bgColor, axisesColor, range_):
         self.displaySize = displaySize
         self.bgColor = bgColor
+        self.axisesColor = axisesColor
         self.range_ = range_
 
 
     def plot(self):
         pygame.init()
-        surface = pygame.display.set_mode(self.displaySize)
+        self.surface = pygame.display.set_mode(self.displaySize)
         pygame.display.set_caption("diit_pz1911_y2_cg1_safonov")
 
         while True:
-            surface.fill(self.bgColor)
+            self.surface.fill(self.bgColor)
             self.axises()
             self.function()
 
@@ -30,13 +31,24 @@ class Plot:
 
 
     def function(self):
-        return
+        pygame.draw.lines(
+                self.surface,
+                self.axisesColor,
+                False,
+                (
+                    (0, 0),
+                    (0, self.surface.get_height()),
+                    self.surface.get_size()
+                    ),
+                10
+                )
 
 
 def main():
     plot_ = Plot(
                 (800, 600),
                 (255, 255, 255),
+                (0, 0, 0),
                 (-10, 10)
             )
     plot_.plot() 
