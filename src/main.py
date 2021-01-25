e19eca2e63c2cda6b61db0edf80982ab0b21075b
dazzlemon <daanill13470@gmail.com>
2021-01-25 11:47:25 +0200

    fix

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame
 from numpy import arange
 from functools import reduce
+from math import sin
 
 
 def linear_map(x, from_, to):
@@ -11,11 +12,12 @@ def virtual_map(height):
 
 
 class Plot:
-    def __init__(self, displaySize, bgColor, axisesColor, funColor, range_, f):
+    def __init__(self, displaySize, bgColor, axisesColor, funColor, textColor, range_, f):
         self.displaySize = displaySize
         self.bgColor = bgColor
         self.axisesColor = axisesColor
         self.funColor = funColor
+        self.textColor = textColor
         self.range_ = range_
         self.f = f
 
@@ -41,7 +43,7 @@ class Plot:
     def function(self):
         points = [(x, self.f(x)) for x in arange(self.range_[0], self.range_[1], (self.range_[1] - self.range_[0]) / self.surface.get_width())]
         rangeY = reduce(
-                lambda old, point: (min(old[0], point[0]), max(old[1], point[1])),
+                lambda old, point: (min(old[0], point[1]), max(old[1], point[1])),
                 points,
                 (points[0][1], points[0][1])
             )
@@ -66,6 +68,7 @@ class Plot:
                 list(points),
                 10
                 )
+        #print("rangeY( " + str(rangeY[0]) + " , " + str(rangeY[1]) + " )")
         return rangeY
 
 
@@ -86,9 +89,10 @@ class Plot:
                 (self.surface.get_width(), zeroY),
                 10
             )
-        textX = self.font.render("x", True, self.axisesColor)
-        textY = self.font.render("y", True, self.axisesColor)
-        textZero = self.font.render("0", True, self.axisesColor)
+
+        textX = self.font.render("x", True, self.textColor)
+        textY = self.font.render("y", True, self.textColor)
+        textZero = self.font.render("0", True, self.textColor)
 
         self.surface.blit(textY, (zeroX + 10, 0))
         self.surface.blit(textX, (self.surface.get_width() - 32, zeroY - 10 - 32))
@@ -97,12 +101,13 @@ class Plot:
 
 def main():
     plot_ = Plot(
-                (800, 600),
-                (255, 255, 255),
-                (0, 0, 0),
-                (0, 0, 0),
-                (-10, 10),
-                lambda x: x
+                displaySize = (800, 600),
+                bgColor = (255, 255, 255),
+                axisesColor = (0, 255, 0),
+                funColor = (0, 0, 255),
+                textColor = (255, 0, 0),
+                range_ = (-10, 10),
+                f = sin
             )
     plot_.plot() 
 
