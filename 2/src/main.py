9732900c46865c21bcebb6539a4699ee2839d1db
dazzlemon <daanill13470@gmail.com>
2021-02-23 18:58:01 +0200

    added comments for frogs component

--- 2/src/main.py
+++ 2/src/main.py
@@ -28,19 +28,19 @@ class Cg2(QApplication):
 
         self._canvas = QCanvas(self._scene)
         self._items = [
-            GraphicsEllipse(400, 400, 400, 300, QColor(60, 150, 0)),
-            GraphicsCircle(450, 350, 200, QColor(60, 150, 0)),
-            GraphicsCircle(580, 350, 200, QColor(60, 150, 0)),
-            GraphicsEllipse(500, 420, 120, 55, QColor(255, 255, 255)),
-            GraphicsEllipse(650, 420, 120, 55, QColor(255, 255, 255)),
-            GraphicsCircle(550, 420, 50, QColor(0, 0, 0)),
-            GraphicsCircle(700, 420, 50, QColor(0, 0, 0)),
-            GraphicsRect(560, 430, 30, 20, QColor(255, 255, 255)),
-            GraphicsRect(710, 430, 30, 20, QColor(255, 255, 255)),
-            GraphicsSquare(560, 430, 10, QColor(0, 0, 0)),
-            GraphicsSquare(710, 430, 10, QColor(0, 0, 0)),
-            GraphicsEllipse(500, 550, 300, 55, QColor(30, 80, 0)),
-            GraphicsLine(500, 580, 300, QColor(0, 0, 0))
+            GraphicsEllipse(400, 400, 400, 300, QColor(60, 150, 0)),#   head body
+            GraphicsCircle(450, 350, 200, QColor(60, 150, 0)),#         left eye body
+            GraphicsCircle(580, 350, 200, QColor(60, 150, 0)),#         right eye body
+            GraphicsEllipse(500, 420, 120, 55, QColor(255, 255, 255)),# left white
+            GraphicsEllipse(650, 420, 120, 55, QColor(255, 255, 255)),# right white
+            GraphicsCircle(550, 420, 50, QColor(0, 0, 0)),#             left pupil
+            GraphicsCircle(700, 420, 50, QColor(0, 0, 0)),#             right pupil
+            GraphicsRect(560, 430, 30, 20, QColor(255, 255, 255)),#     left flare
+            GraphicsRect(710, 430, 30, 20, QColor(255, 255, 255)),#     right flare
+            GraphicsSquare(560, 430, 10, QColor(0, 0, 0)),#             left flare shadow
+            GraphicsSquare(710, 430, 10, QColor(0, 0, 0)),#             right flare shadow
+            GraphicsEllipse(500, 550, 300, 55, QColor(30, 80, 0)),#     mouth
+            GraphicsLine(500, 580, 300, QColor(0, 0, 0))#               lip line
         ]
         self._about_point = GraphicsPoint(600, 600)
         self._update()
@@ -98,7 +98,6 @@ class Cg2(QApplication):
         def scene_click(event):
             self._about_point.x = event.scenePos().x()
             self._about_point.y = event.scenePos().y()
-            print(self._about_point.x, self._about_point.y)# DEBUG
             self._update()
         self._scene.mouseReleaseEvent = scene_click
 
