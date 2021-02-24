9eab73b343061a2e4709609573627675a53fc6c1
dazzlemon <daanill13470@gmail.com>
2021-02-24 17:14:43 +0200

    class PeppeEye

--- 2/src/peppe_frog.py
+++ 2/src/peppe_frog.py
@@ -6,18 +6,44 @@ class PeppeFrog(IDrawable, IAffineTransformable):
     def __init__(self):
         self._items = [
             GraphicsEllipse(400, 400, 400, 300, QColor(60, 150, 0)),#   head body
-            GraphicsCircle(450, 350, 200, QColor(60, 150, 0)),#         left eye body
-            GraphicsCircle(580, 350, 200, QColor(60, 150, 0)),#         right eye body
-            GraphicsEllipse(500, 420, 120, 55, QColor(255, 255, 255)),# left white
-            GraphicsEllipse(650, 420, 120, 55, QColor(255, 255, 255)),# right white
-            GraphicsCircle(550, 420, 50, QColor(0, 0, 0)),#             left pupil
-            GraphicsCircle(700, 420, 50, QColor(0, 0, 0)),#             right pupil
-            GraphicsRect(560, 430, 30, 20, QColor(255, 255, 255)),#     left flare
-            GraphicsRect(710, 430, 30, 20, QColor(255, 255, 255)),#     right flare
-            GraphicsSquare(560, 430, 10, QColor(0, 0, 0)),#             left flare shadow
-            GraphicsSquare(710, 430, 10, QColor(0, 0, 0)),#             right flare shadow
             GraphicsEllipse(500, 550, 300, 55, QColor(30, 80, 0)),#     mouth
-            GraphicsLine(500, 580, 300, QColor(0, 0, 0))#               lip line
+            GraphicsLine(500, 580, 300, QColor(0, 0, 0)),#              lip line
+            PeppeEye(150),# right eye
+            PeppeEye(0),# left eye
+        ]
+
+
+    def move(self, delta: IPoint):
+        """IAffineTransformable override"""
+        for i in self._items:
+            i.move(delta)
+
+
+    def rotate(self, about: IPoint, rad: float):
+        """IAffineTransformable override"""
+        for i in self._items:
+            i.rotate(about, rad)
+
+
+    def scale(self, about: IPoint, w: float, h: float):
+        """IAffineTransformable override"""
+        for i in self._items:
+            i.scale(about, w, h)
+
+
+    def paint(self, canvas: ICanvas):
+        for i in self._items:
+            i.paint(canvas)
+
+
+class PeppeEye(IDrawable, IAffineTransformable):
+    def __init__(self, x: float):
+        self._items = [
+            GraphicsCircle(450 + x, 350, 200, QColor(60, 150, 0)),#         left eye body
+            GraphicsEllipse(500 + x, 420, 120, 55, QColor(255, 255, 255)),# left white
+            GraphicsCircle(550 + x, 420, 50, QColor(0, 0, 0)),#             left pupil
+            GraphicsRect(560 + x, 430, 30, 20, QColor(255, 255, 255)),#     left flare
+            GraphicsSquare(560 + x, 430, 10, QColor(0, 0, 0)),#             left flare shadow
         ]
 
 
