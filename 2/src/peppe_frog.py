6f8096d8bf8ab1f7411e17ca811d3d47a507e836
dazzlemon <daanill13470@gmail.com>
2021-02-24 17:21:43 +0200

    class PeppeMouth & class CompoundGraphics

--- 2/src/peppe_frog.py
+++ 2/src/peppe_frog.py
@@ -2,69 +2,55 @@ from graphics._interfaces import IDrawable, IAffineTransformable, IPoint, ICanva
 from graphics.items import GraphicsEllipse, GraphicsCircle, GraphicsRect, GraphicsSquare, GraphicsLine
 from PyQt5.QtGui import QColor
 
-class PeppeFrog(IDrawable, IAffineTransformable):
+
+class CompoundGraphics(IDrawable, IAffineTransformable):
+    def move(self, delta: IPoint):
+        """IAffineTransformable override"""
+        for i in self._items:
+            i.move(delta)
+
+
+    def rotate(self, about: IPoint, rad: float):
+        """IAffineTransformable override"""
+        for i in self._items:
+            i.rotate(about, rad)
+
+
+    def scale(self, about: IPoint, w: float, h: float):
+        """IAffineTransformable override"""
+        for i in self._items:
+            i.scale(about, w, h)
+
+
+    def paint(self, canvas: ICanvas):
+        for i in self._items:
+            i.paint(canvas)
+
+
+class PeppeFrog(CompoundGraphics):
     def __init__(self):
         self._items = [
-            GraphicsEllipse(400, 400, 400, 300, QColor(60, 150, 0)),#   head body
-            GraphicsEllipse(500, 550, 300, 55, QColor(30, 80, 0)),#     mouth
-            GraphicsLine(500, 580, 300, QColor(0, 0, 0)),#              lip line
-            PeppeEye(150),# right eye
-            PeppeEye(0),# left eye
+            GraphicsEllipse(400, 400, 400, 300, QColor(60, 150, 0)),# head body
+            PeppeEye(150),#                                           right eye
+            PeppeEye(0),#                                             left eye
+            PeppeMouth()
         ]
 
 
-    def move(self, delta: IPoint):
-        """IAffineTransformable override"""
-        for i in self._items:
-            i.move(delta)
-
-
-    def rotate(self, about: IPoint, rad: float):
-        """IAffineTransformable override"""
-        for i in self._items:
-            i.rotate(about, rad)
-
-
-    def scale(self, about: IPoint, w: float, h: float):
-        """IAffineTransformable override"""
-        for i in self._items:
-            i.scale(about, w, h)
-
-
-    def paint(self, canvas: ICanvas):
-        for i in self._items:
-            i.paint(canvas)
-
-
-class PeppeEye(IDrawable, IAffineTransformable):
+class PeppeEye(CompoundGraphics):
     def __init__(self, x: float):
         self._items = [
-            GraphicsCircle(450 + x, 350, 200, QColor(60, 150, 0)),#         left eye body
-            GraphicsEllipse(500 + x, 420, 120, 55, QColor(255, 255, 255)),# left white
-            GraphicsCircle(550 + x, 420, 50, QColor(0, 0, 0)),#             left pupil
-            GraphicsRect(560 + x, 430, 30, 20, QColor(255, 255, 255)),#     left flare
-            GraphicsSquare(560 + x, 430, 10, QColor(0, 0, 0)),#             left flare shadow
+            GraphicsCircle(450 + x, 350, 200, QColor(60, 150, 0)),#         eye body
+            GraphicsEllipse(500 + x, 420, 120, 55, QColor(255, 255, 255)),# white
+            GraphicsCircle(550 + x, 420, 50, QColor(0, 0, 0)),#             pupil
+            GraphicsRect(560 + x, 430, 30, 20, QColor(255, 255, 255)),#     flare
+            GraphicsSquare(560 + x, 430, 10, QColor(0, 0, 0)),#             flare shadow
         ]
 
 
-    def move(self, delta: IPoint):
-        """IAffineTransformable override"""
-        for i in self._items:
-            i.move(delta)
-
-
-    def rotate(self, about: IPoint, rad: float):
-        """IAffineTransformable override"""
-        for i in self._items:
-            i.rotate(about, rad)
-
-
-    def scale(self, about: IPoint, w: float, h: float):
-        """IAffineTransformable override"""
-        for i in self._items:
-            i.scale(about, w, h)
-
-
-    def paint(self, canvas: ICanvas):
-        for i in self._items:
-            i.paint(canvas)
+class PeppeMouth(CompoundGraphics):
+    def __init__(self):
+        self._items = [
+            GraphicsEllipse(500, 550, 300, 55, QColor(30, 80, 0)),# lips
+            GraphicsLine(500, 580, 300, QColor(0, 0, 0)),#          lip line
+        ]
