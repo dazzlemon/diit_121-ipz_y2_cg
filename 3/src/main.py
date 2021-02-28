f3a11dfeb53f7318a5436ad0f3d5be4d3b28870a
dazzlemon <daanill13470@gmail.com>
2021-02-28 20:01:44 +0200

    temporary projection 3d->2d

--- 3/src/main.py
+++ 3/src/main.py
@@ -2,11 +2,21 @@
 Cg3 Main
 """
 
+from math import sqrt
+import numpy as np
 from PyQt5.QtGui import QPen, QBrush
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene
 from graphics_point3d import Point3d, GraphicsPoint3d
 from forms_py import Ui_MainWindow
+
+t_right_angle_dimetric = np.array([
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 1],
+    [0, 0, -1, 0],
+])
+
 
 class Cg3(QApplication):
     def __init__(self, argv):
@@ -21,13 +31,13 @@ class Cg3(QApplication):
 
     def _init_canvas(self):
         self._scene = QGraphicsScene()
-        self._scene.setSceneRect(0, 0, 800, 600)
+#        self._scene.setSceneRect(0, 0, 800, 600)
         self._main_ui.gview.setScene(self._scene)
-        self._main_ui.gview.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+ #       self._main_ui.gview.setAlignment(Qt.AlignTop | Qt.AlignLeft)
 
         self.parallelepiped = Parallelepiped(
-            GraphicsPoint3d(0, 0, 0),
-            GraphicsPoint3d(0, 0, 100),
+            GraphicsPoint3d(0, 0,   0),
+            GraphicsPoint3d(0, 100, 0),
             100,
             100
         )
@@ -54,6 +64,8 @@ class Parallelepiped:
         """
         start1 & start2 - starting points for bases (that are connected by single edge)
         w, h - size of base
+        w -> O_x
+        h -> O_z
         """
 
         self.start1 = start1
@@ -66,17 +78,20 @@ class Parallelepiped:
         points = [
             self.start1,
             GraphicsPoint3d(self.start1.x + self.w, self.start1.y,          self.start1.z),
-            GraphicsPoint3d(self.start1.x + self.w, self.start1.y + self.h, self.start1.z),
-            GraphicsPoint3d(self.start1.x,          self.start1.y + self.h, self.start1.z),
+            GraphicsPoint3d(self.start1.x + self.w, self.start1.y, self.start1.z + self.h),
+            GraphicsPoint3d(self.start1.x,          self.start1.y, self.start1.z + self.h),
             self.start2,
             GraphicsPoint3d(self.start2.x + self.w, self.start2.y,          self.start2.z),
-            GraphicsPoint3d(self.start2.x + self.w, self.start2.y + self.h, self.start2.z),
-            GraphicsPoint3d(self.start2.x,          self.start2.y + self.h, self.start2.z),
+            GraphicsPoint3d(self.start2.x + self.w, self.start2.y, self.start2.z + self.h),
+            GraphicsPoint3d(self.start2.x,          self.start2.y, self.start2.z + self.h),
         ]
 
         for p in points:
-            p_ = p
-            canvas.addEllipse(p_.x, p_.y, 4, 4, QPen(), QBrush())
+            p4 = np.array([p.x, p.y, p.z, 1])
+            p4_ = t_right_angle_dimetric @ p4
+            x = (p.x - p.z) / sqrt(2)#p4_[0]
+            y = (p.x + 2*p.y + p.z) / sqrt(6)#p4_[1]
+            canvas.addEllipse(x, y, 4, 4, QPen(), QBrush())
 
 
 def main():
