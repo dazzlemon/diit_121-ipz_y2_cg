b867fc1d85ed9b8c62bde544e922abcdc2fb6e89
dazzlemon <daanill13470@gmail.com>
2021-03-04 16:36:44 +0200

    refac

--- 3/src/graphics_point3d.py
+++ 3/src/graphics_point3d.py
@@ -5,13 +5,6 @@ CG3 GraphicsPoint3d
 from math import sin, cos, pi, sqrt
 import numpy as np
 
-t_right_angle_dimetric = np.array([
-    [ 0.707, -0.408, 0, 0],
-    [ 0,      0.816, 0, 0],
-    [-0.707, -0.408, 0, 0],
-    [ 0,      0,     0, 1],
-])
-
 
 class Point2d:
     def __init__(self, x, y):
@@ -135,17 +128,35 @@ class GraphicsPoint3d(Point3d):
         return "(%s, %s, %s)" % (self.x, self.y, self.z)
 
 
-def world3d_to_2d(p: Point3d) -> Point2d:
+#def axonometric_proj(p: Point3d) -> Point2d:
+    #t = np.array([
+    #    [0, 0, 0, 0],
+    #    [0, 0, 0, 0],
+    #    [0, 0, 0, 0],
+    #    [0, 0, 0, 0],
+    #])
     #p4 = np.array([p.x, p.y, p.z, 1])
-    #p4_ = t_right_angle_dimetric @ p4
+    #p4_ = t @ p4
     #y = p4_[1]
     #x = p4_[0]
-    #x = (p.x - p.z) / sqrt(2)#p4_[0]
-    #y = -(p.x - 2*p.y + p.z) / sqrt(6)#p4_[1]
+    #return Point2d(x, y)
+
+
+def dimetric_proj(p: Point3d) -> Point2d:
     _42 = np.deg2rad(42)
     _7  = np.deg2rad(7)
     x = p.x*cos(_7) -  p.z*cos(_42)/2
     y = p.y - p.z*sin(_42)/2 - p.x*sin(_7)
+    return Point2d(x, y)
+
+
+def world3d_to_2d(p: Point3d) -> Point2d:
+    return dimetric_proj(p)
+
+
+def isometric_proj(p: Point3d) -> Point2d:
+    x =  (p.x         - p.z)/sqrt(2)
+    y = -(p.x - 2*p.y + p.z)/sqrt(6)
     return Point2d(x, y)
 
 
