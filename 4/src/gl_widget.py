10ecda58d000575a11fd5a6dd95d42f354b95681
dazzlemon <daanill13470@gmail.com>
2021-04-19 18:40:57 +0300

    fill still works

--- 4/src/gl_widget.py
+++ 4/src/gl_widget.py
@@ -16,7 +16,7 @@ class OpenGLCanvas(ICanvas):
     @staticmethod
     def draw_lines(points: List[IPoint], color: QColor):
         """draws lines between i-th and (i-1)-th points"""
-        glColor3i(color.red(), color.green(), color.blue())
+        glColor3f(color.red() / 255, color.green() / 255, color.blue() / 255)# color 3i was showing wrong color
         glBegin(GL_LINES)
         for p1, p2 in pairwise(points):
             glVertex2f(p1.x, p1.y)
@@ -27,38 +27,14 @@ class OpenGLCanvas(ICanvas):
     @staticmethod
     def fill(points: List[IPoint], color: QColor):
         """fills all pixels in space closed by lines between i-th and (i-1)-th points"""
-        glColor3i(color.red(), color.green(), color.blue())
+        glColor3f(color.red() / 255, color.green() / 255, color.blue() / 255)# color 3i was showing wrong color
         glPolygonMode(GL_FRONT, GL_FILL)# polygon rasterization mode
 
-        glBegin(GL_POLYGON)# next vertices will be grouped into triangles
+        glBegin(GL_POLYGON)# next vertices will represent a single polygon
         for p1, p2 in pairwise(points):
             glVertex2f(p1.x, p1.y)
             glVertex2f(p2.x, p2.y)
-        glEnd()# end glBegin(GL_TRIANGLES)
-
-
-    @staticmethod
-    def paint_shape():
-        """TMP"""
-        glColor3f(1.0, 1.5, 0.0)# color for next vertices
-        glPolygonMode(GL_FRONT, GL_FILL)# polygon rasterization mode
-
-        glBegin(GL_TRIANGLES)# next vertices will be grouped into triangles
-        glVertex3f(2.0, -1.2, -1.0)
-        glVertex3f(2.6,  0.0, 5.0)
-        glVertex3f(2.9, -1.2, -4.0)
-        glEnd()# end glBegin(GL_TRIANGLES)
-
-
-    @staticmethod
-    def paint_gl():
-        """TMP"""
-        glClearColor(1, 1, 1, 1)
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glLoadIdentity()# identity matrix
-        glTranslatef(-2.5, 0.5, -6.0)# move (0, 0, 0)
-        OpenGLCanvas.paint_shape()
-        glFlush()
+        glEnd()# end glBegin(GL_POLYGON)
 
 
     @staticmethod
